db29f6e30629aadde15d881ddcd0e695514c0b55
KINGLORD <bharathkumar5325@gmail.com>
2026-09-12 17:42:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: 'https://lmec-ncc-attendance.vercel.app',
   credentials: true
 }));
 app.use(express.json());
